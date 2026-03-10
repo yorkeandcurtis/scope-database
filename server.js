5746c5202324a6a6cb7cacb17417f69012d083c0
Erik Timmons <erik@Eriks-Mac-mini.local>
2026-03-10 10:53:55 -0700

    Add password protection via basic auth

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const basicAuth = require('express-basic-auth');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(basicAuth({
+  users: { [process.env.AUTH_USER || 'yc']: process.env.AUTH_PASS || 'changeme' },
+  challenge: true,
+  realm: 'Yorke & Curtis Scope Database'
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
